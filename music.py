c4355dbb590d7dcf398a0b07f132d58a15d6f6c0
Ducky <124077142+ducky4life@users.noreply.github.com>
2026-02-07 22:49:46 +0800

    fix title+artist search

--- music.py
+++ music.py
@@ -127,7 +127,10 @@ async def search_songs(filter:str="title", query:str="None"):
     elif filter == "album":
         songs = [song['file_path'] for song in song_dicts if query.lower() in str(song['album']).lower()]
     elif filter == "title_artist":
-        songs = [song['file_path'] for song in song_dicts if query.lower() in f"{str(song['title']).lower()} {str(song['artist']).lower()}"]
+        query_list = query.split(",")
+        title = query_list[0]
+        artist = query_list[1]
+        songs = [song['file_path'] for song in song_dicts if title.lower() in str(song['title']).lower() and artist.lower() in str(song['artist']).lower()]
 
     return(songs)
 
